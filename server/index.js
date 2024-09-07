53afcf64a3b476bc3947c84d782ade1de5b7fb36
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-09-07 20:12:20 +0530

    connect to the mongodb atals

--- server/index.js
+++ server/index.js
@@ -2,14 +2,18 @@ const { urlencoded } = require("express");
 const express = require("express");
 const app = express();
 
+const dotenv = require("dotenv");
 app.use(urlencoded({ extended: true }));
-
 app.use(express.json());
+
+const { connectDB } = require("./db/connectedDB.js");
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("dlkbcfdbcdj");
 });
 
 app.listen(3000, () => {
+  connectDB();
   console.log(" thi port is on the 3000");
 });
